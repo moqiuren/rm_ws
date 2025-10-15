89a4e19706645dc2b18699aab9ce2e54b89cfbd3
平山诗音 <2477960254@qq.com>
2025-10-15 08:16:36 +0800

    移除不必要的 build、install、log 目录

--- build/pu_and_su_cpp/CMakeFiles/pu_and_su_cpp_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pu_and_su_cpp_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pu_and_su_cpp_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/pu_and_su_cpp/CMakeFiles/pu_and_su_cpp_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for pu_and_su_cpp_uninstall.
-# This may be replaced when dependencies are built.
